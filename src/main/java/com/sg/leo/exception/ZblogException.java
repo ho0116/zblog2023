bc655d45df3ffd7ffc97f7d21586760ba7e3e4a5
310-1 <310-1@310-31>
2023-05-25 15:11:21 +0900

    플레인자바 회원가입성공

---
+++ src/main/java/com/sg/leo/exception/ZblogException.java
@@ -0,0 +1,9 @@
+package com.sg.leo.exception;
+
+
+public class ZblogException extends RuntimeException{
+	private static final long SeriaLVersionUID = 1L;
+	public ZblogException(String message) {
+		super(message);
+	}
+}
