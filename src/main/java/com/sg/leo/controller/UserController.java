594c7c2d1cadcfa3721457dd03c6d914da5a8f7c
310-1 <310-1@310-31>
2023-06-01 13:55:14 +0900

    jquery 회원가입

--- src/main/java/com/sg/leo/controller/UserController.java
+++ src/main/java/com/sg/leo/controller/UserController.java
@@ -3,6 +3,7 @@ package com.sg.leo.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,20 +14,22 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.sg.leo.domain.RoleType;
 import com.sg.leo.domain.User;
+import com.sg.leo.dto.ResponseDTO;
 import com.sg.leo.exception.ZblogException;
 import com.sg.leo.repository.UserRepository;
+import com.sg.leo.service.UserService;
 
 @Controller
 public class UserController {
 	@Autowired
 	private UserRepository userRepository;
 	
-	@PostMapping("/user")
-	public @ResponseBody String insertUser(@RequestBody User user) {
-		user.setRole(RoleType.USER);
-		userRepository.save(user);
-		return user.getUsername() + "님 회원가입성공";
-	}
+//	@PostMapping("/user")
+//	public @ResponseBody String insertUser(@RequestBody User user) {
+//		user.setRole(RoleType.USER);
+//		userRepository.save(user);
+//		return user.getUsername() + "님 회원가입성공";
+//	}
 	
 	@GetMapping("/user/get/{id}")
 	public @ResponseBody User getUer(@PathVariable int id) {
@@ -50,5 +53,14 @@ public class UserController {
 	@GetMapping("/auth/insertUser")
 	public String insertUser() {
 		return "user/insertUser";
+	}
+	
+	@Autowired
+	private UserService userService;
+	
+	@PostMapping("/auth/insertUser")
+	public @ResponseBody ResponseDTO<?> insertUser(@RequestBody User user){
+		userService.insertUser(user);
+		return new ResponseDTO<>(HttpStatus.OK.value(), user.getUsername() + "님 회원가입 성공했어요");
 	}
 }
--- src/main/resources/static/js/user.js
+++ src/main/resources/static/js/user.js
@@ -1,3 +1,4 @@
+/*
 function usersubmit() {
 	alert("회원가입 요청됨:old style javascript");
 
@@ -29,3 +30,43 @@ function memberjoin() {
 }
 
 memberjoin();
+
+*/
+
+ let userObject = {
+    init: function(){
+        let _this = this;
+
+        $("#btn-save").on("click",() =>{
+            _this.insertUser();
+        });
+    },
+
+    insertUser: function() {
+		alert("회원가입 요청됨");
+		// 회원정보 추출
+		let user = {
+			username: $("#username").val(),
+			password: $("#password").val(),
+			email: $("#email").val()
+		}
+		
+		console.log(user);
+
+		$.ajax({
+			type: "POST",
+			// 시험으로는 /user로 함
+			url: "/auth/insertUser",
+			data: JSON.stringify(user),
+			contentType: "application/json; charset=utf-8"
+		}).done(function(response) {
+			console.log(response);
+			alert(response);
+			location = "/";
+		}).fail(function(error) {
+			alert("!/js/user.js에서 에러발생: " + error);
+		});
+	},
+}
+
+ userObject.init();
